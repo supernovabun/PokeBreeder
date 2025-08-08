529157fb63233b6b41b2ec05942c031ab834effe
supernovabun <supernovabun@gmail.com>
2025-08-08 10:43:46 -0400

    Added an item to play with

--- worldgen.py
+++ worldgen.py
@@ -6,6 +6,8 @@ def generate_world():
 	generate_stages()
 	master_bedroom = Room("Master bedroom in a quaint house")
 	master_bedroom.set_verbose("A cozy room bearing no hints of profession, there isn't much space to move around. A bed covered in maroon blankets sits against the wall, underneath the lone window in the room. Next to it is a small wooden night table, leaving just enough room to climb into bed. The floor is soft cream-colored carpet, and the walls, a faint baby blue.")
+	starting_backpack = Item("A worndown leather backpack", "Misc.", master_bedroom, 50, container=True)
+	starting_backpack.set_desc("Leather makes up the majority of this dark brown backpack. It features one large main pocket with a flap and snap for closure, and another smaller pocket on the front with another snap for closure. Two straps that have begun to fray allow for easy wear.")
 
 	main_hallway = Room("A narrow hallway")
 	main_hallway.set_verbose("There is little room to maneuver through this hallway. Getting around requires walking sideways and even then the average person or Pokemon is prone to bumping into the wall or passing doorknob. Against all wisdom, a painting is hung here - a testament to the intelligence of its hanger as it requires ducking to get past.")
