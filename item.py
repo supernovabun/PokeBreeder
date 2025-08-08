2e6fadfa4259cf52fadf477f135ffe610df2348c
supernovabun <supernovabun@gmail.com>
2025-08-08 10:39:25 -0400

    Added functions get_article, add_inventory, remove_inventory

--- item.py
+++ item.py
@@ -9,6 +9,7 @@ class Item:
 		self.base_value = int(value)
 		self.gettable = gettable
 		self.container = container
+		self.inventory = [] if container else None
 		if len(Item.item_id.keys()) == 0:
 			self.item_id = f"{name.title()}"+"{:04d}".format(1)
 		else:
@@ -24,5 +25,44 @@ class Item:
 		self.description = desc
 
 	def get_desc(self):
+		if self.container:
+			counts = {}
+			for i in sorted(self.inventory):
+				if i[:-4] in counts.keys():
+					counts[i[:-4]] += 1
+				else:
+					counts[i[:-4]] = 1
+			counts = [f"  {"{:03d}".format(v)} ... {k}" for k, v in counts.items()]
+			counts = "\n".join(counts)
+			counts = counts if len(counts) > 0 else "  Nothing"
+			return(self.description+"\nIt holds the following:\n"+counts)
+		else:
+			return(self.description)
 
-		return(self.description)
+	def get_article(self):
+		if self.name.split(" ")[0] in ["The", "A", "An"]:
+			article = ""
+		elif self.name[0] in ["A", "E", "I", "O", "U"]:
+			article = "an "
+		else:
+			article = "a "
+		return(article)
+
+	def add_inventory(self, to_add):
+		if self.container:
+			if self.inventory:
+				self.inventory.append(to_add.item_id)
+			else:
+				self.inventory = [to_add.item_id]
+			print(f"You carefully place {to_add.get_article()}{to_add.name.lower()} inside of {self.get_article()}{self.name.lower()}.")
+		else:
+			print(f"{self.name[0]+self.name[1:].lower()} will not fit {to_add.get_article()}{to_add.name.lower()}, no matter how hard you may try.")
+
+	def remove_inventory(self, to_remove):
+		if self.container:
+			if self.inventory:
+				self.inventory.pop(self.inventory.index(to_remove))
+			else:
+				print(f"{self.name} has nothing inside of it.")
+		else:
+			print(f"{self.name} cannot and does not hold anything inside of it.")
