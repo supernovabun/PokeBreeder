bdc824d6906b97a219df983c32df8c75953a68a4
supernovabun <supernovabun@gmail.com>
2025-08-09 18:39:53 -0400

    This now checks for a new player, how cool is that - no idea how it works but it does

--- worldgen.py
+++ worldgen.py
@@ -2,8 +2,15 @@ from room import Room
 from item import Item
 from indiv import *
 
-def generate_world():
+def generate_world(player_type, player):
 	generate_stages()
+	if player_type == "new":
+		starting_stage = new_world(player)
+	return(starting_stage)
+
+def new_world(player):
+	print("Welcome to the wonderful world of Pokemon! Through some method or another to be decided later, you got a Pokemon! You're getting a Bulbasaur, to be precise.")
+
 	home_master_bedroom = Room("Master bedroom in a quaint house")
 	home_master_bedroom.set_verbose("A cozy room bearing no hints of profession, there isn't much space to move around. A bed covered in maroon blankets sits against the wall, underneath the lone window in the room. Next to it is a small wooden night table, leaving just enough room to climb into bed. The floor is soft cream-colored carpet, and the walls, a faint baby blue.")
 	starting_backpack = Item("A worndown leather backpack", "Misc.", home_master_bedroom, 50, container=True)
@@ -23,6 +30,7 @@ def generate_world():
 	home_upstairs_tub = Item("Tiny tub for a smaller person", "Furniture", home_upstairs_bathroom, 100000, gettable=False, container=True)
 	home_upstairs_tub.set_desc("Every effort has been made to try to maintain the cleanliness of a tub made for someone far smaller than the average adult human. The white interior is stained, and the metal handles and faucet, rusty. Marks are present indicating that someone tried at least once to remove the grime and age from the tub.")
 	home_upstairs_tub.inventory = [home_upstairs_rubber_ducklett.item_id]
+	home_upstairs_rubber_ducklett.in_somewhere = home_upstairs_tub
 	
 	home_stairs_top = Room("The top of the stairs")
 	home_stairs_top.set_verbose("Brought on a draft, cool air makes its way up a half-turned flight of oak stairs. On one side is a bannister with chipped wood, and the other, a wall covered in floral wallpaper. Much like the bannister, the wooden floor here is chipped, waiting for the chance to lodge pieces of itself in some unsuspecting foot.")
@@ -31,6 +39,14 @@ def generate_world():
 	home_main_hallway.set_exits({"west": home_master_bedroom, "north": home_stairs_top, "east": home_upstairs_bathroom})
 	home_upstairs_bathroom.set_exits({"west": home_main_hallway})
 	home_stairs_top.set_exits({"south": home_main_hallway})
+
+	### TEST ###
+	mom = Ivysaur("Mommy", sex="F")
+	dad = Venusaur("Daddy", sex="M")
+	testBulba = mom.breed(dad)
+	testBulba.set_trainer(player) ## TEST
+	player.add_pokemon(testBulba) ## TEST
+	testBulba.set_room(home_master_bedroom)
 	return(home_master_bedroom)
 
 def generate_stages():
