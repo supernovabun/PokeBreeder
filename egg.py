94852fa45bc95799eb06f48dde92723dced3cd04
supernovabun <supernovabun@gmail.com>
2026-01-13 08:43:52 -0500

    Overdue update to Egg

--- egg.py
+++ egg.py
@@ -5,12 +5,12 @@ from pokemon_registry import *
 class Egg:
     egg_id = {}
     egg_num = 1
-    def __init__(self, species, room, mother=None, father=None, days_remaining=100, egg_id=None):
+    def __init__(self, species, room, mother=None, father=None, steps_remaining=100, egg_id=None):
         self.species = species  # e.g. Bulbasaur
         self.name = "Egg"
         self.mother = mother if isinstance(mother, str) or mother == None else mother.pkmn_id
         self.father = father if isinstance(father, str) or father == None else father.pkmn_id
-        self.days_remaining = days_remaining
+        self.steps_remaining = steps_remaining
         self.held_by = None
         if mother and not isinstance(mother, str):
             self.trainer = mother.trainer
@@ -36,13 +36,35 @@ class Egg:
         if not self.held_by:
             new_room.add_inventory(self.egg_id)
 
+    def get_desc(self):
+        desc = self.description
+        status = ""
+        if self.steps_remaining >= 100:
+            status = " It will be a while before it hatches. What could be inside?"
+        elif self.steps_remaining >= 75:
+            status = " It's getting closer to hatching and sometimes it moves around."
+        elif self.steps_remaining >= 50:
+            status = " A single crack has made itself known on the surface."
+        elif self.steps_remaining >= 25:
+            status = " Many small cracks splinter off from the larger cracks on its surface."
+        elif self.steps_remaining >= 0:
+            status = " It's covered in cracks and hints of the egg's membrane are visible within. It's going to hatch at any moment!"
+        return(self.description + status)
+
+
     def hatch(self):
         species_cls = pokemon_registry.get(self.species)
         if not species_cls:
             raise ValueError(f"Species '{self.species}' is not registered.")
         if list(Trainer.trainer_id.values())[0].room == self.room:
             print(f"You watch in amazement as a {self.species} hatches from an egg right before your very eyes!")
-        return(species_cls(sex="r",pattern="parents",mother=self.mother,father=self.father,hatched_from_egg=True))
+        hatched_pokemon = species_cls(sex="r",pattern="parents",mother=self.mother,father=self.father,hatched_from_egg=True)
+        hatched_pokemon.room = self.room
+        self.room.add_inventory(hatched_pokemon.pkmn_id)
+        hatched_pokemon.set_trainer(self.trainer)
+        if self.held_by:
+            self.trainer.remove_inventory(self.egg_id)
+        return(hatched_pokemon)
 
     def set_held(self, holder):
         if not holder:
@@ -60,12 +82,13 @@ class Egg:
             "mother": self.mother,
             "father": self.father,
             "holder": self.held_by,
+            "steps": self.steps_remaining,
             "room": self.room if isinstance(self.room, str) or self.room == None else self.room.room_id,
-            "desc": self.description,
+            "desc": self.description
         })
 
     @staticmethod
     def from_dict(data):
-        loaded_egg = Egg(data["species"], data["room"], mother=data["mother"], father=data["father"], egg_id=data["egg_id"])
+        loaded_egg = Egg(data["species"], data["room"], mother=data["mother"], father=data["father"], steps_remaining=data["steps"], egg_id=data["egg_id"])
         loaded_egg.set_held(data["holder"])
         return(loaded_egg)
